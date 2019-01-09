9957c1c963b8764d4af599d41cc0e5b87f77dded
WXL <wangxl1029@sina.com>
2019-01-09 22:10:16 +0800

    New project: tabbed app2 for the coding tabbed.

---
+++ TabbedApp2/TabbedApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabbedApp2
+//
+//  Created by alan king on 2019/1/9.
+//  Copyright © 2019年 Allen Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
