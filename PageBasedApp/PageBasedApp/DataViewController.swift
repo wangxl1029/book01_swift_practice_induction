2328ba483faad7ba36bdac61db653c635bec7840
WXL <wangxl1029@sina.com>
2019-01-10 18:59:31 +0800

    New project: a initial page-based app on iOS from app wizard.

---
+++ PageBasedApp/PageBasedApp/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  PageBasedApp
+//
+//  Created by alan king on 2019/1/10.
+//  Copyright © 2019年 Allen Lab. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
