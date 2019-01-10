2380421360cbf9562b9c41f794f7ac24efb7ff62
WXL <wangxl1029@sina.com>
2019-01-10 21:39:54 +0800

    New project: generate the tabbed app by the iOS app wizard.

---
+++ TabbedApp/TabbedApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabbedApp
+//
+//  Created by alan king on 2019/1/10.
+//  Copyright © 2019年 Allen Lab. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
