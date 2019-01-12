9e0ab03747cdeecdbf217c2cb7e1fd3236d78da3
WXL <wangxl1029@sina.com>
2019-01-12 22:50:27 +0800

    New feature: add a label control to the corresponding view for detail and a coding button. 1) 细节视图的北景色由黑色改变成白色。 2) 添加在细节视图清除控件的方法和显示控件的方法（只有label控件）。 3) 细节视图上方导航栏的右侧按钮可在“代码”和“效果”间相互切换，但“代码”的细节视图还没有显示任何代码。

--- NaviRootViewApp/NaviRootViewApp/DetailViewController.swift
+++ NaviRootViewApp/NaviRootViewApp/DetailViewController.swift
@@ -15,17 +15,52 @@ class DetailViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         // 设置背景颜色
-        self.view.backgroundColor = UIColor.black
+        self.view.backgroundColor = UIColor.white
+        // 按title加载控件
+        loadControl(ctrl: self.title!)
+        
         // 设置代码和控件展示切换按钮，增加到导航条右侧
         // 这里采用了navigationController不能增加navigationItem
         let btn = UIBarButtonItem(title: "代码", style: .plain, target: self, action: #selector(DetailViewController.btnCodeClicked(sender:)))
         self.navigationItem.rightBarButtonItem = btn
     }
     
+    // 显示控件代码
+    func loadControl(ctrl: String) {
+        switch ctrl {
+        case "UILabel":
+            let label = UILabel(frame: self.view.bounds)
+            label.backgroundColor = UIColor.clear
+            label.textAlignment = .center
+            label.font = UIFont.systemFont(ofSize: 36)
+            label.text = "Hello, Ucai"
+            self.view.addSubview(label)
+        default:
+            print("control name: \(ctrl)")
+        }
+    }
+
+    // 清空所有子视图
+    func clearViews() {
+        for v in self.view.subviews as [UIView] {
+            v.removeFromSuperview()
+        }
+    }
+    
+    // 显示按钮控件
     @objc
     func btnCodeClicked(sender: AnyObject) {
         print("title: \(self.title!)")
+        clearViews()
+        
+        if self.navigationItem.rightBarButtonItem!.title! == "代码" {
+            self.navigationItem.rightBarButtonItem!.title = "效果"
+        }else{
+            self.navigationItem.rightBarButtonItem!.title = "代码"
+            loadControl(ctrl: self.title!)
+        }
     }
+    
     /*
     // MARK: - Navigation
 
