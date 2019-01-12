fc5c8cbe83f4c11174bf01514feaa15d38acde55
WXL <wangxl1029@sina.com>
2019-01-12 15:34:54 +0800

    New project: the initial single view app for the bar and controller of navigation.

---
+++ NaviRootViewApp/NaviRootViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NaviRootViewApp
+//
+//  Created by alan king on 2019/1/12.
+//  Copyright © 2019年 Allen Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
